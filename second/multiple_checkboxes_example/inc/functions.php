efd005fffdfca8de91f356a1af836f1547c64939
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-06-29 16:41:16 +0100

    Checkboxes example

---
+++ second/multiple_checkboxes_example/inc/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function checked($needle, $haystack)
+{
+	if ($haystack) {
+		return in_array($needle, $haystack) ? 'checked' : '';
+	}
+
+	return '';
+}
+
+?>
---
+++ second/multiple_checkboxes_example/inc/header.php
@@ -0,0 +1,12 @@
+<!DOCUMENT html>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, inicial-scale=1.0" />
+    <link href="css/style.css" rel="stylesheet"  />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding:wght@400;700&display=swap" rel="stylesheet">
+    <title>PHP - Multiple Checkboxes - Pizza Toppings</title>
+</head>
+<body class="center">
+    <main>
+        <p><img src="img/pizza.png" width="72" height="72" title="Pizza Toppings"></p>
