cd95775fcb39abb3a4f063e3a5a3a499163943c4
iurecastro <55067724+iurecastro@users.noreply.github.com>
2024-06-29 16:50:37 +0100

    Add files via upload

--- second/multiple_checkboxes_example/index.php
+++ second/multiple_checkboxes_example/index.php
@@ -11,6 +11,22 @@ $pizza_toppings = [
     'onions' => 1.5,
     'sausage' => 2.5,
     'bacon' => 1,
+    'extra cheese' => 1.5,
+    'black olives' => 1,
+    'green peppers' => 1,
+    'pineapple' => 2,
+    'spinach' => 1.5,
+    'tomatoes' => 1,
+    'chicken' => 2.5,
+    'jalapenos' => 1,
+    'anchovies' => 2,
+    'artichokes' => 2,
+    'ham' => 2,
+    'salami' => 2,
+    'garlic' => 0.5,
+    'basil' => 0.5,
+    'prosciutto' => 3,
+    'roasted red peppers' => 1.5,
 ];
 
 $request_method = $_SERVER['REQUEST_METHOD'];
